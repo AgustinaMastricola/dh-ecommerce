6920412ed4dcbcbd2798b5bbec54bf6ffde67989
Agustina Mastricola <agus.ppye@gmail.com>
2023-10-11 12:34:21 -0300

    Card products components

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react"
 import Hero from "../../components/ui/hero/Hero"
+import styles from './Home.module.css'
+import CardProduct from "../../components/ui/card/CardProduct"
 
 const Home = () => {
+  const [products, setProducts] = useState([])
+
+  const getProducts = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/products')
+      const data = await response.json()
+      setProducts(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
   return (
     <>
       <Hero/>
+      <div className={styles.container}>
+        {products.map((product) => (
+          <CardProduct key={product.id} product={product}/>  
+        ))}
+      </div>
     </>
   )
 }
